cb4c5ab0f6184141d9056a776a8b7fdf830bf339
edarvichm <edarvichm@gmail.com>
2023-03-28 22:51:38 -0300

    creación estilo y js base

---
+++ assets/js/mainCRUD.js
@@ -0,0 +1,18 @@
+function createPopup() {
+    const listActivites = document.getElementById("listActivites");
+    const form = document.getElementById("form");
+
+    if (listActivites.classList.contains("show")) {
+        // Agrega clase para mostrar el popup
+        form.classList.add("show");
+
+        // Quita clase para ocultar la lista de actividades
+        listActivites.classList.remove("show");
+    } else {
+        // Quita clase para ocultar el popup
+        form.classList.remove("show");
+
+        // Agrega clase para mostrar la lista de actividades
+        listActivites.classList.add("show");
+    }
+}
