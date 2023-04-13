a613b97028795b61f4a3e611c3934a405b66ade7
edarvichm <edarvichm@gmail.com>
2023-04-12 21:41:42 -0400

    Ajuste responsividad y correccion tooltip

--- assets/js/toolTips.js
+++ assets/js/toolTips.js
@@ -10,17 +10,18 @@ for (let i = 0; i < tds.length; i++) {
   // Agregar evento para el toque (touchstart)
   td.addEventListener("touchstart", function (e) {
     const tooltip = document.createElement("div");
-    tooltip.className = "tooltip";
+    tooltip.className = "tooltip1";
     tooltip.textContent = this.getAttribute("alt"); // Usar el contenido del td actual
     const touchX = e.changedTouches[0].clientX;
     const touchY = e.changedTouches[0].clientY;
     tooltip.style.top = touchY + "px";
-    tooltip.style.left = touchX + 10 + "px";
-    if (this.getAttribute("alt") != "btns") document.body.appendChild(tooltip);
+    tooltip.style.left = touchX + "px";
+    if (this.getAttribute("alt") != "btns") 
+    document.querySelector(".main-content") .appendChild(tooltip);
   }, { passive: true });
   // Agregar evento para el toque (touchend)
   td.addEventListener("touchend", function () {
-    const tooltip = document.querySelector(".tooltip");
+    const tooltip = document.querySelector(".tooltip1");
     if (tooltip) {
       tooltip.parentNode.removeChild(tooltip);
     }
@@ -34,20 +35,22 @@ for (let i = 0; i < tds.length; i++) {
   // Evento para el clic (mouseover)
   td.addEventListener("mouseover", function (e) {
     const tooltip = document.createElement("div");
-    tooltip.className = "tooltip";
+    tooltip.className = "tooltip1";
     tooltip.textContent = this.getAttribute("alt"); // Usar el atributo alt del td actual
     //   const rect = this.getBoundingClientRect();
     //   tooltip.style.top = rect.top + 'px';
     //   tooltip.style.left = rect.left + 'px';
     let mouseX = e.clientX;
     let mouseY = e.clientY;
-    tooltip.style.top = mouseY + "px";
+    tooltip.style.top = mouseY +50 + "px";
     tooltip.style.left = mouseX + 10 + "px";
-    if (this.getAttribute("alt") != "btns") document.body.appendChild(tooltip);
+    if (this.getAttribute("alt") != "btns") 
+    document.querySelector(".main-content").appendChild(tooltip);
+    // document.body.appendChild(tooltip);
   });
   // Evento para el mouse fuera de la celda (mouseout)
   document.addEventListener("mouseout", function (e) {
-    const tooltip = document.querySelector(".tooltip");
+    const tooltip = document.querySelector(".tooltip1");
     if (tooltip && e.target !== tooltip) {
       tooltip.parentNode.removeChild(tooltip);
     }
